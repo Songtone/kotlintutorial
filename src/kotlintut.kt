b618f2ce5618d4b4627c4031fc24625c4307d733
Sebastien Ong Tone <s.ongtone@hotmail.com>
2019-01-12 15:53:04 -0500

    start

---
+++ src/kotlintut.kt
@@ -0,0 +1,14 @@
+package demo
+
+fun main(args : Array<String>){
+    println("Hello, World!")
+
+    val name = "Seb"
+
+    var age = 28
+
+    var bigInt: Int = Int.MAX_VALUE
+    var smallInt: Int = Int.MIN_VALUE
+
+    println("My name is: $name and I am $age years old. I like $bigInt and I hate $smallInt.")
+}
